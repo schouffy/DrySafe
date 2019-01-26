6ee04a53fd720832d4ff7fff44631aa4f0ae2af6
jerome.scheefer <jerome.scheefer@gmail.com>
2019-01-26 18:56:08 +0100

    Added logging and email alerts when errors occur

--- DrySafe/DrySafe.cs
+++ DrySafe/DrySafe.cs
@@ -20,13 +20,37 @@ namespace DrySafe
         var rainStatus = WeatherHelper.GetRainInNextHour();
         if (rainStatus.WillItRain)
         {
+          DrySafe.Logger.Info($"Rain is planned ! Sending notification.");
           NotificationHelper.SendNotification(rainStatus.GetDescription());
+        }
+        else
+        {
+          DrySafe.Logger.Info($"No rain planned.");
         }
       }
       catch (Exception e)
       {
         Logger.Error($"Error: {e.Message}");
-        NotificationHelper.SendNotification($"DrySafe - Erreur: {e.Message}");
+        try
+        {
+          NotificationHelper.SendNotification($"DrySafe - Erreur: {e.Message}");
+        }
+        catch (Exception e2)
+        {
+          MailHelper.SendMail("DrySafe: Error", "One or more errors occured during DrySafe execution: "
+            + Environment.NewLine
+            + e.Message
+            + Environment.NewLine
+            + e.StackTrace
+            + Environment.NewLine
+            + Environment.NewLine
+            + e2.Message
+            + Environment.NewLine
+            + e2.StackTrace
+            + Environment.NewLine
+            + Environment.NewLine
+            ).Wait();
+        }
       }
     }
   }
